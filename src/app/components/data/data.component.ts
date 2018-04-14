74b77becab4607d7629c4b8131b3f5a4382f526f
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-04-14 14:13:19 +0200

    Reset y carga de datos al formulario a partir de un objet

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -9,8 +9,8 @@ import {FormGroup, FormControl, Validators} from '@angular/forms';
 export class DataComponent {
 
   form: FormGroup;
-  usuario: Object = {
-    nombreCompleto: {
+  usuario: any = {
+    nombrecompleto: {
       nombre: "Jose",
       apellido: "Palacios"
     },
@@ -20,15 +20,18 @@ export class DataComponent {
   constructor() {
     this.form = new FormGroup({
       'nombrecompleto': new FormGroup({
-        'nombre': new FormControl('Jose', [Validators.required, ]),
+        'nombre': new FormControl('', [Validators.required, ]),
         'apellido': new FormControl('', [Validators.required, ]),
       }),
       'correo': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$") ])
     })
+
+    this.form.setValue( this.usuario );
   }
 
   guardar(){
     console.log( this.form.value, this.form, this.form.valid);
+    this.form.reset({})
   }
 
 
