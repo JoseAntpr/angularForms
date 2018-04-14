26a9e2ce9c146cee208453c8d22c94d1f924ffe6
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-04-14 14:02:41 +0200

    Agrupaciones de los objetos: FormGroupName

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -9,11 +9,20 @@ import {FormGroup, FormControl, Validators} from '@angular/forms';
 export class DataComponent {
 
   form: FormGroup;
+  usuario: Object = {
+    nombreCompleto: {
+      nombre: "Jose",
+      apellido: "Palacios"
+    },
+    correo: "joseantpalacios@gmail.com"
+  }
 
-  constructor() { 
+  constructor() {
     this.form = new FormGroup({
-      'nombre': new FormControl('Jose', [Validators.required, ]),
-      'apellido': new FormControl('', [Validators.required, ]),
+      'nombrecompleto': new FormGroup({
+        'nombre': new FormControl('Jose', [Validators.required, ]),
+        'apellido': new FormControl('', [Validators.required, ]),
+      }),
       'correo': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$") ])
     })
   }
